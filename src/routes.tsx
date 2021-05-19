4771e1a7f1c69ee8a5f8fce1864d4b2883076e21
Gppenna <gpenna@ufba.br>
2021-05-19 14:16:48 -0300

    Versão provisória

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Processes from './pages/Processes'
 
 const Routes: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const Routes: React.FC = () => {
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/register" component={Register} exact />
+        <Route path="/processes" component={Processes} exact />
       </Switch>
     </Router>
   )

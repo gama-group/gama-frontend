198ab1a4ccf24d95b6a9beb8ed479ab4837ab923
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2021-05-25 19:58:43 -0300

    Adds contractor private area

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Processes from './pages/Processes'
+import PrivateArea from './pages/PrivateArea'
 
 import { PublicRoute, ProtectedRoute, PrivateRoute } from './components/Routes'
 
@@ -24,7 +25,9 @@ const Routes: React.FC = () => {
         <PublicRoute path="/" exact component={Home} />
         <ProtectedRoute path="/login" component={Login} />
         <ProtectedRoute path="/register" component={Register} />
-        <PrivateRoute path="/processes" component={Processes} />
+        <PrivateRoute path="/processes/create" component={Processes} />
+        <PrivateRoute path="/processes/edit" component={Processes} />
+        <PrivateRoute path="/processes" component={PrivateArea} />
       </Switch>
     </Router>
   )
